441ae273a31e01811c54c0f63e815c158d803aca
Salar Abbasi <salaaar@hamravesh.com>
2017-11-23 08:32:10 +0330

    Fix propTypes bug

--- PDFView.android.js
+++ PDFView.android.js
@@ -1,5 +1,5 @@
 'use strict';
-import React,{ Component, PropTypes } from 'react';
+import React, { Component } from 'react';
 import { requireNativeComponent, View } from 'react-native';
 
 class PDFView extends Component {
@@ -12,27 +12,24 @@ class PDFView extends Component {
     this._root.setNativeProps(nativeProps);
   }
 
-  _onChange(event:Event) {
-    this.props.onLoadComplete && this.props.onLoadComplete(Number(event.nativeEvent.message));
+  _onChange(event: Event) {
+    this.props.onLoadComplete &&
+      this.props.onLoadComplete(Number(event.nativeEvent.message));
   }
 
   render() {
-    return <PDFCustomView ref={component => this._root = component} {...this.props} onChange={this._onChange}/>;
+    return (
+      <PDFCustomView
+        ref={component => (this._root = component)}
+        {...this.props}
+        onChange={this._onChange}
+      />
+    );
   }
 }
 
-PDFView.propTypes = {
-  ...View.propTypes,
-    asset: PropTypes.string,
-    src: PropTypes.string,
-    pageNumber: PropTypes.number,
-    path: PropTypes.string,
-    zoom: PropTypes.number,
-    onLoadComplete: PropTypes.func
-};
-
 var PDFCustomView = requireNativeComponent('RCTPDFViewAndroid', PDFView, {
-  nativeOnly: {onChange: true}
+  nativeOnly: { onChange: true },
 });
 
 export default PDFView;
